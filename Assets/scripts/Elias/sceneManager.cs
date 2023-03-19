96e329f9d88f89e4c8f3ef10bb37210d5719ef6a
Mpfv-Sus <90852111+Mpfv-Sus@users.noreply.github.com>
2023-03-19 15:56:24 +0100

    waxm

--- Assets/scripts/Elias/sceneManager.cs
+++ Assets/scripts/Elias/sceneManager.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void nextLevel()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
